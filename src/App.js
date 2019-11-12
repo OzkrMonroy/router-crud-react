c53bb5a2484ed44061137137e4db6f82d33c0ced
Oscar Monroy <ozkrmonroy65@gmail.com>
2019-11-12 13:42:02 -0600

    routes are ready

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import Header from './components/Header'
+import Productos from './components/Productos'
+import EditarProducto from './components/EditarProducto'
+import AgregarProducto from './components/AgregarProducto'
+import Producto from './components/Producto'
 
 function App() {
+  // Siempre colocar las rutas más específicas primero luego los genéricos (/:id)
+
   return (
-    <div className="App">
-      
-    </div>
+    <Router>
+      <Header/>
+      <main className="container mt-5">
+        <Switch>
+          <Route exact path="/nuevo-producto" component={AgregarProducto}/>
+          <Route exact path="/productos" component={Productos}/>
+          <Route exact path="/productos/:id" component={Producto}/>
+          <Route exact path="/productos/editar/:id" component={EditarProducto}/>
+        </Switch>
+      </main>
+      <p className="mt4 p2 text-center">Todos los derechos reservados</p>
+    </Router>
   );
 }
 
